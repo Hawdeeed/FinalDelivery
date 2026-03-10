546c0ad23cc85fcb93e7c4900937fbd3cd8600c9
hawdeeed <hadeeda980@gmail.com>
2026-03-10 17:46:04 +0500

    mock commit

--- backend2.py
+++ backend2.py
@@ -105,8 +105,6 @@ def generate_style_image(base_image: str, reference_image: str, flow_type: int, 
         prompt = (
                 "Keep the watch exactly the same as in reference image 1 — identical size, scale, zoom level, camera distance, framing, position, dial, dial interior, orientation, and strap. Do not zoom in or out. Only change the background, lighting, and ambiance to match reference image 2. Preserve the original composition. The generated image must have the same width and height as reference image 1."
             )
-
-        # ✅ FIXED ORDER
         fal_response = nano_banana_edit(
             prompt,
             resolution,
@@ -116,7 +114,6 @@ def generate_style_image(base_image: str, reference_image: str, flow_type: int, 
         prompt = (
             "Extract the style from Image B and put that style on Image A without changing identical size, scale, zoom level, camera distance, framing, position, dial, dial interior, orientation, wrist, wrist position and strap in Image A."
             )
-
         # ✅ FIXED ORDER
         fal_response = nano_banana_edit(
             prompt,
